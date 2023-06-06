af48318cd40bd8eb84af14b33ffe713517e18b01
Ferliness <reason.life@yandex.ru>
2023-06-06 14:09:48 +0300

    Add: Second step.

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,15 +2,17 @@
 
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser(
-                    description='Compares two configuration files and shows a difference.',
-                    epilog='Made by Ferliness.')
+                description="""Compares two configuration
+                                files and shows a difference.""",
+                epilog='Made by Ferliness.'
+            )
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    args = parser.parse_args()
-    
-    print('Hello, World!')
+    parser.add_argument('-f', '--format', help='set format of output')
+    parser.parse_args()
 
 
 if __name__ == '__main__':
